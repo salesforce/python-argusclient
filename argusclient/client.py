3178842566ee9682083d7c1ff55a34e6fd0cbc21
Pasha Kuznetsov <pkuznetsov@salesforce.com>
2018-01-18 23:59:32 -0800

    Dummy commit to force salesforce-cla check...

--- argusclient/client.py
+++ argusclient/client.py
@@ -7,7 +7,7 @@ and `web service reference <https://github.com/SalesforceEng/Argus/wiki/Web%20Se
 #
 # Copyright (c) 2016, salesforce.com, inc.
 # All rights reserved.
-# Licensed under the BSD 3-Clause license.
+# Licensed under the BSD 3-Clause license. 
 # For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
 #
 
