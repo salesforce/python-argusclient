b3d490f984246e999e021305940246f43d958ba4
Hari Krishna Dara <hdara@salesforce.com>
2021-08-28 13:30:19 +0530

    Update argusclient/model.py

--- argusclient/model.py
+++ argusclient/model.py
@@ -507,8 +507,8 @@ class JsonDecoder(json.JSONDecoder):
     def from_json(self, jsonObj):
         if not jsonObj or not isinstance(jsonObj, dict):
             return jsonObj
-        for cls in (
-        Metric, Dashboard, AddListResult, User, Namespace, Annotation, Alert, Trigger, Notification, Permission):
+        for cls in (Metric, Dashboard, AddListResult, User, Namespace, Annotation,
+                    Alert, Trigger, Notification, Permission):
             obj = cls.from_dict(jsonObj)
             if obj:
                 return obj
